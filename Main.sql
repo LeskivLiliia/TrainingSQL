7e31765376b89609965ff359abcfaad27b06fe59
Liliia Leskiv <llesk@softserveinc.com>
2021-05-18 10:21:32 +0300

    add task12.1 - reporting

--- Main.sql
+++ Main.sql
@@ -99,6 +99,45 @@ FROM DAYS_SALARIES
 GO
 
 ----------------------------------------------------------
+--TASK 12.1 Report with time interval
+----------------------------------------------------------
+DROP PROCEDURE IF EXISTS uspTask12_Report
+GO
+
+CREATE PROCEDURE uspTask12_Report @StartDate DATE, @EndDate DATE
+								
+AS
+	BEGIN
+		DROP TABLE IF EXISTS ##Dates;
+
+		WITH Date_CTE
+		AS
+		(
+			SELECT [Date] = @StartDate 
+			UNION ALL 
+			SELECT [Date] = DATEADD(DAY, 1, [Date])
+			FROM Date_CTE WHERE [Date] < @EndDate
+		)
+
+		SELECT [Date] INTO ##Dates
+		FROM Date_CTE OPTION (MAXRECURSION 0)
+		
+
+
+		SELECT D.FULL_NAME, D.Date, DS.SALARY FROM DAYS_SALARIES AS DS
+		RIGHT JOIN 
+		(
+			SELECT DISTINCT FULL_NAME, ##Dates.Date 
+			FROM DAYS_SALARIES
+			CROSS JOIN ##Dates
+		) AS D
+		ON DS.DATE = D.Date AND DS.FULL_NAME = D.FULL_NAME
+	END
+GO
+
+EXEC uspTask12_Report @StartDate='2020-08-21', @EndDate='2020-08-26'
+
+----------------------------------------------------------
 --TASK 13
 ----------------------------------------------------------
 --Return JSON
